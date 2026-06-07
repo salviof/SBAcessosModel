cb600e146442fa67b45b107577c34c43d5d42a3a
salviof <salviof>
2026-06-07 16:36:06 -0300

    Atualizacao automática

--- src/main/java/config/ConfigPermissoesAcessosModel.java
+++ src/main/java/config/ConfigPermissoesAcessosModel.java
@@ -6,8 +6,13 @@
 package config;
 
 import com.super_bits.modulos.SBAcessosModel.ConfigPermissoesAcessoModelAbstrato;
+import com.super_bits.modulos.SBAcessosModel.controller.FabMenuAdmin;
+import com.super_bits.modulos.SBAcessosModel.fabricas.FabAcaoProjetoSB;
+import com.super_bits.modulos.SBAcessosModel.fabricas.acoesDemonstracao.FabAcaoDemonstracaoSB;
+import com.super_bits.modulosSB.SBCore.ConfigGeral.CarameloCode;
 import com.super_bits.modulosSB.SBCore.modulos.Controller.Interfaces.permissoes.ErroDadosDeContatoUsuarioNaoEncontrado;
 import com.super_bits.modulosSB.SBCore.modulos.erp.FabTipoAgenteOrganizacao;
+import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.ItensGenericos.basico.UsuarioSistemaRoot;
 
 import org.coletivojava.fw.api.objetoNativo.view.menu.MenusDaSessao;
 import org.coletivojava.fw.api.objetoNativo.view.menu.MenuSBFW;
@@ -15,6 +20,7 @@ import com.super_bits.modulosSB.SBCore.modulos.objetos.entidade.basico.ComoGrupo
 import com.super_bits.modulosSB.SBCore.modulos.objetos.entidade.basico.ComoUsuario;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.entidade.contato.ComoContatoHumano;
 import com.super_bits.modulosSB.SBCore.modulos.view.menu.ComoMenusDeSessao;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.StackType;
 
 /**
  * \
@@ -28,13 +34,18 @@ public abstract class ConfigPermissoesAcessosModel extends ConfigPermissoesAcess
         return classes;
     }
 
+    public ConfigPermissoesAcessosModel(Class[] pClassesControllers) {
+        super(pClassesControllers);
+    }
+
     public ConfigPermissoesAcessosModel() {
         super(getClasses());
     }
 
     @Override
     public ComoMenusDeSessao definirMenu(ComoGrupoUsuario pGrupo) {
-        return new MenusDaSessao(new MenuSBFW());
+
+        return new MenusDaSessao(pGrupo.getModuloPrincipal().getEnumVinculado().getMenuPadrao().getMenuPrincipal(), pGrupo.getModuloPrincipal().getEnumVinculado().getMenuPadrao().getMenuSecundario());
     }
 
 }
